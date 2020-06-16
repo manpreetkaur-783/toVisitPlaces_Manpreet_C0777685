726506ff7f5b1402fb44ba75898da525ef5d62a0
manpreetkaur-783 <66217528+manpreetkaur-783@users.noreply.github.com>
2020-06-16 17:38:43 -0400

    added constant.swift

---
+++ constant.swift
@@ -0,0 +1,13 @@
+//
+//  constant.swift
+//  toVisitPlaces_Manpreetkaur_C0777685
+//
+//  Created by user175413  on 15/06/20.
+//  Copyright © 2020 user175413 . All rights reserved.
+//
+
+import UIKit
+
+var AppName = "FindWay_Manpreet_C0777685"
+
+
